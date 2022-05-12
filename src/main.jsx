11cb7452ac9870f3822e91e1d5a0a477ccd8e954
Matej Drlicka <sprattek@gmail.com>
2022-05-12 11:04:08 +0200

    js lib added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <App />
+  </Router>
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,9 +21,9 @@ function Home() {
         {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
-        <FeaturesBlocks />
+        {/*<FeaturesBlocks />
         <Testimonials />
-        <Newsletter />
+        <Newsletter />*/}
 
       </main>
 
